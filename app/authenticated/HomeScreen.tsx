0a2000d0758d4ddae416ec24d5865defb5cc30b9
Michal Hajduga <mjhajduga@gmail.com>
2024-11-10 15:59:23 +0100

    Restructuring and create index.js

--- app/authenticated/HomeScreen.tsx
+++ app/authenticated/HomeScreen.tsx
@@ -1,9 +1,69 @@
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import { View, Text } from "react-native";
+import CustomDrawerContent from "./DrawerContent";
+import TabBar from "./TabBar";
+import colors from "../../shared/enums/colors";
 
-export default function HomeScreen() {
+const Tab = createBottomTabNavigator();
+
+const Drawer = createDrawerNavigator();
+
+function EmptyComponent() {
+  return <View />;
+}
+
+function SearchScreen() {
   return (
-    <View style={{ marginTop: 100 }}>
-      <Text>Home screen</Text>
+    <View>
+      <Text>SearchScreen</Text>
     </View>
+  );
+}
+
+function HomeScreen(): JSX.Element {
+  return (
+    <Tab.Navigator
+      tabBar={(props) => <TabBar {...props} />}
+      screenOptions={{ headerShown: false, tabBarHideOnKeyboard: true }}
+    >
+      <Tab.Screen name="Search screen" component={SearchScreen} />
+      <Tab.Screen
+        name="Drawer"
+        listeners={({ navigation }) => ({
+          tabPress: (e) => {
+            e.preventDefault();
+            navigation.openDrawer();
+          },
+        })}
+        component={EmptyComponent}
+      />
+    </Tab.Navigator>
+  );
+}
+
+export default function HomeScreenWithDrawer({ navigation }) {
+  console.log('rendering HomeScreenWithDrawer')
+  return (
+    <Drawer.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headerShown: false,
+        drawerLabelStyle: {
+          color: "white",
+          fontFamily: "Wimba",
+          fontSize: 16,
+        },
+        drawerStyle: {
+          backgroundColor: colors.darkBlue,
+        },
+      }}
+      drawerContent={(props) => (
+        <CustomDrawerContent {...props} stackNavigation={navigation} />
+      )}
+    >
+      <Drawer.Screen name={'Home'} component={HomeScreen} />
+    </Drawer.Navigator>
   );
 }
